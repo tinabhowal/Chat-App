d74adf2a5381595b6b485ab955b70dad091196f1
tinabhowal <117632464+tinabhowal@users.noreply.github.com>
2023-05-03 22:32:57 +0200

    Task 5.2

--- components/Chat.js
+++ components/Chat.js
@@ -1,25 +1,78 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
   const { name, selectedColor } = route.params;
+  const [messages, setMessages] = useState([]);
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+
+  //Changing the color of renderBubble
+  const renderBubble = (props) => {
+    return <Bubble
+    // inheriting props
+      {...props}
+      wrapperStyle={{
+        // right: {
+        //   backgroundColor: "#000"
+        // },
+        left: {
+          backgroundColor: "#FFF"
+        }
+      }}
+    />
+  }
 
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, []);
 
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: `Hello ${name}`,
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: 'Welcome to the chat room!',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, [name]);
+
   return (
+    
     <View style={[styles.container, { backgroundColor: selectedColor }]}>
-      <Text>Hello {name}</Text>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
+      { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+      {Platform.OS === "ios"?<KeyboardAvoidingView behavior="padding" />: null}
     </View>
+  
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    // justifyContent: 'center',
+    // alignItems: 'center'
   }
 });
 
--- components/Start.js
+++ components/Start.js
@@ -1,25 +1,167 @@
-import { useState } from 'react';
-import { StyleSheet, View, Text, Button, TextInput, Image, TouchableOpacity } from 'react-native';
-import appBackground from '../assets/appBackground.png';
+// import { useState } from 'react';
+// import { StyleSheet, View, Text, Button, TextInput, Image, TouchableOpacity } from 'react-native';
+// import appBackground from '../assets/appBackground.png';
+
+// const Start = ({ navigation }) => {
+//   const [name, setName] = useState('');
+//   const [selectedColor, setSelectedColor] = useState('#ffffff');
+//   const colorOptions = ['#ffffff', '#e6e6e6', '#bfbfbf', '#808080', '#404040'];
+
+//   return (
+//     <View style={styles.container}>
+//       <Image source={appBackground} style={styles.backgroundImage} />
+//       <Text style={styles.title}>Chat App</Text>
+      
+//       <TextInput
+//         style={styles.textInput}
+//         value={name}
+//         onChangeText={setName}
+//         placeholder='Type your name here'
+//       />
+
+// <Text style={{color: 'white'}}>Choose a background color:</Text>
+//       <View style={styles.colorContainer}>
+//         {colorOptions.map((color, index) => (
+//           <TouchableOpacity
+//             key={index}
+//             style={[styles.colorOption, { backgroundColor: color }]}
+//             onPress={() => setSelectedColor(color)}
+//           />
+//         ))}
+//       </View>
+
+//       <Button
+//         title="Go to chat"
+//         onPress={() => navigation.navigate('Chat', { name: name, selectedColor: selectedColor })}
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center'
+//   },
+//   backgroundImage: {
+//     position: 'absolute',
+//     top: 0,
+//     left: 0,
+//     width: '100%',
+//     height: '100%',
+  
+//   },
+//   title: {
+//     fontSize: 32,
+//     fontWeight: 'bold',
+//     marginBottom: 20,
+//     marginTop: 20,
+//     color: 'white'
+//   },
+//   textInput: {
+//     width: "88%",
+//     padding: 15,
+//     borderWidth: 1,
+//     borderColor: 'white',
+//     marginTop: 15,
+//     marginBottom: 15,
+//     color: 'white',
+//     fontSize: 20
+//   },
+//   colorContainer: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//     width: '80%',
+//     marginBottom: 20,
+//   },
+//   colorOption: {
+//     width: 50,
+//     height: 50,
+//     borderRadius: 25,
+//   }
+// });
+
+
+
+// export default Start;
+
+
+
+
+
+
+
+
+
+import { useState, useRef, useEffect } from 'react';
+import { StyleSheet, View, Text, Button, TextInput, ImageBackground, TouchableOpacity, Animated } from 'react-native';
+import appBackground1 from '../assets/appBackground1.png';
+import appBackground2 from '../assets/appBackground2.png';
+import appBackground3 from '../assets/appBackground3.png';
 
 const Start = ({ navigation }) => {
   const [name, setName] = useState('');
   const [selectedColor, setSelectedColor] = useState('#ffffff');
   const colorOptions = ['#ffffff', '#e6e6e6', '#bfbfbf', '#808080', '#404040'];
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const backgroundImages = [appBackground1, appBackground2, appBackground3, appBackground2];
+
+  useEffect(() => {
+    const fadeIn = Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 2000,
+      useNativeDriver: false,
+    });
+
+    const fadeOut = Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 2000,
+      useNativeDriver: false,
+    });
+
+    
+    
+    const loopImages = () => {
+        if (currentImageIndex === backgroundImages.length - 1) {
+          // Stop the animation and set the current image to be the last image
+          fadeAnim.setValue(1);
+          setCurrentImageIndex(backgroundImages.length - 1);
+          return;
+        }
+      
+        fadeIn.start(() => {
+          setTimeout(() => {
+            fadeOut.start(() => {
+              setCurrentImageIndex((currentImageIndex + 1) % backgroundImages.length);
+            });
+          }, 2000);
+        });
+      };
+      
+      
+
+    loopImages();
+
+    return () => {
+      fadeIn.stop();
+      fadeOut.stop();
+    };
+  }, [currentImageIndex]);
 
   return (
-    <View style={styles.container}>
-      <Image source={appBackground} style={styles.backgroundImage} />
+    <View style={[styles.container, { backgroundColor: backgroundImages[currentImageIndex] }]}>
+      <Animated.Image source={backgroundImages[currentImageIndex]} style={[styles.backgroundImage, { opacity: fadeAnim }]} />
       <Text style={styles.title}>Chat App</Text>
-      
       <TextInput
         style={styles.textInput}
         value={name}
         onChangeText={setName}
         placeholder='Type your name here'
       />
-
-<Text style={{color: 'white'}}>Choose a background color:</Text>
+      <Text style={{color: 'white'}}>Choose a background color:</Text>
       <View style={styles.colorContainer}>
         {colorOptions.map((color, index) => (
           <TouchableOpacity
@@ -29,7 +171,6 @@ const Start = ({ navigation }) => {
           />
         ))}
       </View>
-
       <Button
         title="Go to chat"
         onPress={() => navigation.navigate('Chat', { name: name, selectedColor: selectedColor })}
@@ -38,20 +179,25 @@ const Start = ({ navigation }) => {
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: '#000'
   },
+  
+ 
   backgroundImage: {
     position: 'absolute',
     top: 0,
     left: 0,
     width: '100%',
     height: '100%',
-  
+    backgroundColor: '#000'
   },
+  
   title: {
     fontSize: 32,
     fontWeight: 'bold',
@@ -82,63 +228,4 @@ const styles = StyleSheet.create({
   }
 });
 
-
-
 export default Start;
-
-
-// import React, { useState } from 'react';
-// import { StyleSheet, View, Text, Button, TextInput, Image } from 'react-native';
-// import appBackground from '../assets/appBackground.png';
-// import BackgroundAnimation from './BackgroundAnimation';
-
-// const Start = ({ navigation }) => {
-//   const [name, setName] = useState('');
-
-//   return (
-//     <View style={styles.container}>
-//       <BackgroundAnimation />
-//       <Text style={styles.title}>Chat App</Text>
-//       <TextInput
-//         style={styles.textInput}
-//         value={name}
-//         onChangeText={setName}
-//         placeholder='Type your name here'
-//       />
-//       <Button
-//         title="Go to chat"
-//         onPress={() => navigation.navigate('Chat', { name: name })}
-//       />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   title: {
-//     fontSize: 32,
-//     fontWeight: 'bold',
-//     color: '#fff',
-//     marginBottom: 30,
-//   },
-//   backgroundImage: {
-//     position: 'absolute',
-//     top: 0,
-//     left: 0,
-//     width: '100%',
-//     height: '100%',
-//   },
-//   textInput: {
-//     width: "88%",
-//     padding: 15,
-//     borderWidth: 1,
-//     marginTop: 15,
-//     marginBottom: 15
-//   }
-// });
-
-// export default Start;
